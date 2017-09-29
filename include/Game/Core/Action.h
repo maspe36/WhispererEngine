ef2a841d563b08f02828cedef7d13de8c6553a9b
Sam Privett <privett.sam@gmail.com>
2017-09-29 17:11:19 -0400

    Removed cards and players vectors and forward declared classes because actions will be subclassed heavily

--- include/Game/Core/Action.h
+++ include/Game/Core/Action.h
@@ -5,19 +5,10 @@
 #ifndef WHISPERERENGINE_ACTION_H
 #define WHISPERERENGINE_ACTION_H
 
-
-#include <vector>
-
-class Card;
-class Player;
-
 /* A slice in time of an action to the current game state. */
 class Action
 {
 public:
-    std::vector<Card*> cards;
-    std::vector<Player*> players;
-
     Action();
     ~Action();
 };
