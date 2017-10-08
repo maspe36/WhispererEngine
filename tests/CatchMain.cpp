18b7eb746620f19e3f7237db2607ab03a93e8ffb
Sam Privett <privett.sam@gmail.com>
2017-10-08 00:01:55 -0400

    Added include statement to the catchmain so the define CATCH_CONFIG_MAIN is recognized. Added empty test_Card file.

--- tests/CatchMain.cpp
+++ tests/CatchMain.cpp
@@ -3,3 +3,4 @@
 //
 
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "../build/catch-src/include/catch.hpp"
---
+++ tests/Game/Core/test_Card.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sam on 10/7/2017.
+//
+
+#include "../../../build/catch-src/include/catch.hpp"
