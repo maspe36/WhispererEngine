08f57543778ccd634d8cc6ec935b754ba5a0c8db
maspe36 <privett.sam@gmail.com>
2017-10-19 15:00:59 -0400

    Stubbed out the client header

--- include/Network/Client.h
+++ include/Network/Client.h
@@ -5,12 +5,47 @@
 #ifndef WHISPERERENGINE_CLIENT_H
 #define WHISPERERENGINE_CLIENT_H
 
-#include "boost/asio.hpp"
 
-class Client{
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/asio.hpp>
+#include <boost/algorithm/string.hpp>
+#include <vector>
+
+class Player;
+class Server;
+class Card;
+
+class Client
+        : public boost::enable_shared_from_this<Client>
+{
 public:
-    Client();
-    ~Client();
+    typedef boost::shared_ptr<Client> pointer;
+
+    const std::string delimiter;
+    boost::asio::streambuf buffer;
+    Player* player;
+    Server* server;
+    bool listening;
+
+    static pointer Create(boost::asio::io_service& ioService);
+    boost::asio::ip::tcp::socket& GetSocket();
+
+    void Start(Server* server);
+    void Write(std::string data);
+    void Disconnect();
+    void Listen();
+
+private:
+    boost::asio::ip::tcp::socket socket;
+
+    static std::string GetString(boost::asio::streambuf& buffer);
+    void UserNameReceive(const boost::system::error_code& error);
+    void DeckReceive(const boost::system::error_code& error);
+    void MulliganRecieve(const boost::system::error_code& error);
+    void TurnListen(const boost::system::error_code& error);
+    void OnWrite(const boost::system::error_code& error, size_t bytesTransferred) const;
+
+    explicit Client(boost::asio::io_service& ioService);
 };
 
 
