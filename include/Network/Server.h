793e9ea6e0cb5a46b1f9079648db80ae1bc64137
maspe36 <privett.sam@gmail.com>
2017-10-19 15:00:47 -0400

    Stubbed out the server header

--- include/Network/Server.h
+++ include/Network/Server.h
@@ -6,12 +6,27 @@
 #define WHISPERERENGINE_SERVER_H
 
 
+#include <queue>
+#include "Client.h"
+
+class Player;
+
 class Server
 {
 public:
-    Server();
-    ~Server();
-};
+    std::vector<Client::pointer> clients;
+    std::queue<Player*> queue;
 
+    void WriteToAll(std::string data);
+    Server(boost::asio::io_service& ioService, int port);
+
+private:
+    boost::asio::ip::tcp::acceptor acceptor;
+
+    void Start();
+    void OnAccept(Client::pointer connection, const boost::system::error_code& error);
+
+    static void DoClose(Client::pointer connection);
+};
 
 #endif //WHISPERERENGINE_SERVER_H
