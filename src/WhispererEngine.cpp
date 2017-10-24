e63124003b2cf958c144f80a6dd2e89b12f37e34
Sam Privett <privett.sam@gmail.com>
2017-10-24 13:42:02 -0400

    Removed large clunky rudimentary matchmaking, will redo later.

--- src/WhispererEngine.cpp
+++ src/WhispererEngine.cpp
@@ -19,38 +19,6 @@ int main()
         std::cout << "Starting server..." << std::endl;
         server->Start();
         std::cout << "Server started!" << std::endl;
-
-        // Run the blocking io_service method run()
-        boost::thread ioThread(
-        boost::bind(&boost::asio::io_service::run, &io_service));
-
-        // Consistently try to find games.
-        while (true)
-        {
-//            if (server->queue.size() >= 2)
-//            {
-//                // Get the two players who have been in queue the longest.
-//                std::cout << "Found 2 players in queue!" << std::endl;
-//
-//                std::cout << server->queue.size() << " players in queue!" << std::endl;
-//
-//                // Remove the first 2 players and check if they are still connected.
-//                Player* firstPlayer = server->queue.front();
-//                std::cout << firstPlayer->name << " was selected for a game..." << std::endl;
-//                server->queue.pop();
-//
-//                Player* secondPlayer = server->queue.front();
-//                std::cout << secondPlayer->name << " was selected for a game..." << std::endl;
-//                server->queue.pop();
-//
-//                std::cout << server->queue.size() << " players in queue!" << std::endl;
-//
-//                std::cout << "Starting game between " << firstPlayer->name << " and " << secondPlayer->name << "..." << std::endl;
-//
-//                //Game* newGame = new Game({ firstPlayer, secondPlayer }, server);
-//                //newGame->Start();
-//            }
-        }
     }
     catch (std::exception& e)
     {
