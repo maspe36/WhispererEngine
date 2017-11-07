75197c796fe0ba7fe26ecf7e48995e3a61fbd97e
Sam Privett <privett.sam@gmail.com>
2017-11-07 16:29:14 -0500

    Included the single_include catch.hpp file. Not sure why but it seemed the definitions disappeared from the catch.hpp in the include/ folder

--- tests/CatchMain.cpp
+++ tests/CatchMain.cpp
@@ -2,5 +2,6 @@
 // Created by Sam on 10/7/2017.
 //
 
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "../build/catch-src/include/catch.hpp"
+// This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
+#include "../build/catch-src/single_include/catch.hpp"
