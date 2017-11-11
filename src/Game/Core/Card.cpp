476bbb1742c392331063b4c76150ae238ec12739
Sam Privett <privett.sam@gmail.com>
2017-11-11 12:01:03 -0500

    Linking and building of first python module, Card

--- src/Game/Core/Card.cpp
+++ src/Game/Core/Card.cpp
@@ -3,6 +3,13 @@
 //
 
 #include "../../../include/Game/Core/Card.h"
+#include "../../../include/Game/Core/Mana.h"
+#include "../../../include/Game/Core/Player.h"
+#include "../../../include/Game/Core/Effect.h"
+#include "../../../include/Game/Core/Modifier.h"
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
 
 void Card::addEffect(Effect *effect)
 {
@@ -21,3 +28,16 @@ Card::Card(std::string name, std::string text, Mana *mana)
 
 Card::~Card()
 = default;
+
+PYBIND11_MODULE(Card, m) {
+    py::class_<Card> (m, "Card")
+        .def(py::init<const std::string &, const std::string &, Mana*>())
+        .def_readwrite("name", &Card::name)
+        .def_readwrite("text", &Card::text)
+        .def_readwrite("mana", &Card::mana)
+        .def_readwrite("player", &Card::player)
+        .def_readwrite("effects", &Card::effects)
+        .def_readwrite("modifiers", &Card::modifiers)
+        .def("addEffect", &Card::addEffect)
+        .def("addModifier", &Card::addModifier);
+}
