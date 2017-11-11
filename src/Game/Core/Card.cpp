ac1e52cf15f09cbf6b76ee62126055bee658d236
Sam Privett <privett.sam@gmail.com>
2017-11-11 11:33:04 -0500

    First implementation of card

--- src/Game/Core/Card.cpp
+++ src/Game/Core/Card.cpp
@@ -2,4 +2,24 @@
 // Created by Sam on 9/25/2017.
 //
 
+#include <utility>
+
 #include "../../../include/Game/Core/Card.h"
+
+void Card::addEffect(Effect *effect)
+{
+    effects.push_back(effect);
+}
+
+void Card::addModifier(Modifier *modifier)
+{
+    modifiers.push_back(modifier);
+}
+
+Card::Card(std::string name, std::string text, Mana *mana)
+        : name(std::move(name)), text(std::move(text)), mana(mana)
+{
+}
+
+Card::~Card()
+= default;
