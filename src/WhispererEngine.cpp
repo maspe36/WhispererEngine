6ba9bb5556b99780d3d812dfba11b277f2c64ba3
Sam Privett <privett.sam@gmail.com>
2017-11-20 15:26:30 -0500

    Added testing code to the begining of the normal binary

--- src/WhispererEngine.cpp
+++ src/WhispererEngine.cpp
@@ -5,11 +5,22 @@
 #include "../include/Game/Core/Game.h"
 #include "../include/Game/Core/Player.h"
 #include "../include/Network/Server.h"
+#include "../include/Game/Python/Factory.h"
+#include "../include/Game/Python/Version.h"
 
 #include <iostream>
 
 int main()
 {
+    std::shared_ptr<Factory> factory = std::make_shared<Factory>();
+    std::string minimumVersion = "2.7";
+    bool minimumCheck = Version(minimumVersion) < factory->version;
+    std::string expectedVersion = "3.6.3";
+    bool expectedCheck = Version(expectedVersion) == factory->version;
+
+    std::cout << "minimumCheck: " << minimumCheck << std::endl;
+    std::cout << "expectedCheck: " << expectedCheck << std::endl;
+
     std::cout << "Initializing server..." << std::endl;
     auto* server = new Server(8888);
     std::cout << "Starting server..." << std::endl;
