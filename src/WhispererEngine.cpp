c89e4607e4bb4214a1fdd63c6eb9b36ee91667e7
Sam Privett <privett.sam@gmail.com>
2017-11-25 00:31:24 -0500

    Testing code for creating cards

--- src/WhispererEngine.cpp
+++ src/WhispererEngine.cpp
@@ -5,6 +5,9 @@
 #include "../include/Game/Core/Game.h"
 #include "../include/Game/Core/Player.h"
 #include "../include/Network/Server.h"
+#include "../include/Game/Python/Factory.h"
+#include "../include/Game/Core/Card.h"
+#include "../include/Game/Core/Mana.h"
 
 #include <iostream>
 
@@ -16,10 +19,25 @@ int main()
     server->Start();
     std::cout << "Server started!" << std::endl;
 
+    Factory factory;
+
     // Exit if the user enters quit
     std::string line;
     while (std::getline(std::cin, line) && line != "quit")
     {
+        if (line == "memes")
+        {
+            std::shared_ptr<Card> card = factory.createCard("Azar");
+
+            std::cout << "name: " << card->name << std::endl;
+            std::cout << "text: " << card->text << std::endl;
+            std::cout << "black: " << card->mana->black << std::endl;
+            std::cout << "blue: " << card->mana->blue << std::endl;
+            std::cout << "brown: " << card->mana->brown << std::endl;
+            std::cout << "red: " << card->mana->red << std::endl;
+            std::cout << "green: " << card->mana->green << std::endl;
+            std::cout << "white: " << card->mana->white << std::endl;
+        }
         std::cout << "Unknown command: '" << line << "'" << std::endl;
     }
 
