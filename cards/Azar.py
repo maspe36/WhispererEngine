08b926add38970d626c2e9d92e46163eee013681
Sam Privett <privett.sam@gmail.com>
2017-11-25 17:49:13 -0500

    Removed excess mana cost from Azar

--- cards/Azar.py
+++ cards/Azar.py
@@ -5,5 +5,5 @@ class Azar(Card):
     def __init__(self):
         name = "Azar"
         text = "The first Whisperer card!"
-        mana = Mana(red=1, black=1, blue=1)
+        mana = Mana(red=1)
         super().__init__(name, text, mana)
