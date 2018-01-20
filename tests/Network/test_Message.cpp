4b2974d126e9676ccfe4d9ee8118b851190380dc
Sam Privett <privett.sam@gmail.com>
2018-01-20 14:21:53 -0500

    Added a test for reading in messages from the client.

--- tests/Network/test_Message.cpp
+++ tests/Network/test_Message.cpp
@@ -15,3 +15,15 @@ TEST_CASE("Verify JSON format")
 
     REQUIRE(correctJSON == createdJSON);
 }
+
+TEST_CASE("Parsing JSON")
+{
+    std::string type = "test";
+    std::string data = "Does this test pass?";
+    std::string correctJSON = R"({"type":")" + type + R"(","data":")" + data + R"("})";
+
+    Message message(correctJSON);
+
+    REQUIRE(message.type == type);
+    REQUIRE(message.data == data);
+}
