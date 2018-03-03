e08607e9d8a10a23996bf54bb3e6fb5eb69c28b2
Sam Privett <privett.sam@gmail.com>
2018-03-03 18:40:57 -0500

    Added the new ChatMessage class for handling sending and receiving chat messages.

---
+++ include/Network/Derived/ChatMessage.h
@@ -0,0 +1,21 @@
+//
+// Created by Sam on 3/3/2018.
+//
+
+#ifndef WHISPERERENGINE_CHATMESSAGE_H
+#define WHISPERERENGINE_CHATMESSAGE_H
+
+
+#include "../Message.h"
+
+class ChatMessage
+        : public Message
+{
+public:
+    std::string text;
+
+    explicit ChatMessage(const json& newJSON);
+    ~ChatMessage() override;
+};
+
+#endif //WHISPERERENGINE_CHATMESSAGE_H
