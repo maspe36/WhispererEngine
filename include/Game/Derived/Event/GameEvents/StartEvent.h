6c49e03288ad8c043727c241c41f7dbea8b9c686
Sam Privett <privett.sam@gmail.com>
2018-03-10 17:12:14 -0500

    Added an empty new StartEvent object. Will be created and passed through the effect solver after the game logic for startGame is over

---
+++ include/Game/Derived/Event/GameEvents/StartEvent.h
@@ -0,0 +1,15 @@
+//
+// Created by Sam on 3/10/2018.
+//
+
+#ifndef WHISPERERENGINE_STARTEVENT_H
+#define WHISPERERENGINE_STARTEVENT_H
+
+
+class StartEvent
+{
+
+};
+
+
+#endif //WHISPERERENGINE_STARTEVENT_H
---
+++ src/Game/Derived/Event/GameEvents/StartEvent.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sam on 3/10/2018.
+//
+
+#include "../../../../../include/Game/Derived/Event/GameEvents/StartEvent.h"
