84a3e4bc74c26276178e9ff3b6ca2892c3c711f3
Sam Privett <privett.sam@gmail.com>
2018-03-18 17:28:58 -0400

    Added an implementation to CardMovedMessage to go from shared_ptr<Container> to the zone ID expected and defined by the client

--- include/Network/Derived/CardMovedMessage.h
+++ include/Network/Derived/CardMovedMessage.h
@@ -15,15 +15,13 @@ class CardMovedMessage
         : public Message
 {
 public:
-    std::shared_ptr<Container> origin;
-    std::shared_ptr<Container> destination;
-    std::shared_ptr<Card> card;
-    std::string playerTag;
-
     explicit CardMovedMessage(const std::shared_ptr<Container>& origin,
                               const std::shared_ptr<Container>& destination,
                               const std::shared_ptr<Card>& card);
     ~CardMovedMessage();
+
+private:
+    int getZoneID(const std::shared_ptr<Container>& container);
 };
 
 
--- src/Network/Derived/CardMovedMessage.cpp
+++ src/Network/Derived/CardMovedMessage.cpp
@@ -6,13 +6,43 @@
 #include "../../../include/Game/Core/Card.h"
 #include "../../../include/Game/Core/Container.h"
 #include "../../../include/Game/Core/Player.h"
+#include "../../../include/Game/Derived/Containers/CreatureZone.h"
+#include "../../../include/Game/Derived/Containers/Deck.h"
+#include "../../../include/Game/Derived/Containers/Graveyard.h"
+#include "../../../include/Game/Derived/Containers/Hand.h"
 
 CardMovedMessage::CardMovedMessage(const std::shared_ptr<Container> &origin,
                                    const std::shared_ptr<Container> &destination,
                                    const std::shared_ptr<Card> &card)
-        : origin(origin), destination(destination), card(card), playerTag(card->player->tag)
 {
+    rawJSON[TYPE_KEY] = Message::CARD_MOVED;
+    addDataMember("originZoneID", getZoneID(origin));
+    addDataMember("destinationZoneID", getZoneID(destination));
+    addDataMember("card", card->getJSON());
+    addDataMember("playerTag", card->player->tag);
 }
 
 CardMovedMessage::~CardMovedMessage()
 = default;
+
+int CardMovedMessage::getZoneID(const std::shared_ptr<Container>& container)
+{
+    if (std::dynamic_pointer_cast<Deck>(container))
+    {
+        return 0;
+    }
+    else if (std::dynamic_pointer_cast<Hand>(container))
+    {
+        return 1;
+    }
+    else if (std::dynamic_pointer_cast<CreatureZone>(container))
+    {
+        return 2;
+    }
+    else if (std::dynamic_pointer_cast<Graveyard>(container))
+    {
+        return 3;
+    }
+
+    throw std::runtime_error("Attempted to move a card into an unknown container");
+}
